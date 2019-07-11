dedd29ce12a46bec411f42006e7df4137c9f81c7
CremBluRay <jhoggard2003@gmail.com>
2019-07-10 22:11:58 -0500

    add kick module

--- src/main/java/com/github/coleb1911/ghost2/commands/meta/CommandContext.java
+++ src/main/java/com/github/coleb1911/ghost2/commands/meta/CommandContext.java
@@ -2,14 +2,12 @@ package com.github.coleb1911.ghost2.commands.meta;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Guild;
-import discord4j.core.object.entity.Member;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.MessageChannel;
+import discord4j.core.object.entity.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Contains relevant information for when a command is invoked.
@@ -23,6 +21,7 @@ public class CommandContext {
     private final List<String> args;
     private final String trigger;
     private final DiscordClient client;
+    private final List<User> mentions;
 
     public CommandContext(MessageCreateEvent event) {
         this.guild = event.getGuild().block();
@@ -33,10 +32,7 @@ public class CommandContext {
         this.args = extractArgs(message);
         this.trigger = args.remove(0);
         this.client = event.getClient();
-    }
-
-    public DiscordClient getClient() {
-        return client;
+        this.mentions = event.getMessage().getUserMentions().collect(Collectors.toList()).block();
     }
 
     public Guild getGuild() {
@@ -65,6 +61,14 @@ public class CommandContext {
 
     public String getTrigger() {
         return trigger;
+    }
+
+    public DiscordClient getClient() {
+        return client;
+    }
+
+    public List<User> getMentions() {
+        return mentions;
     }
 
     public void reply(String message) {
