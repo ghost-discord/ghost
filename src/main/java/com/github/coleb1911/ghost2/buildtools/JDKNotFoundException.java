ded56d90fd86f36c410933bdea78f4d2b1c8f2be
cbryant02 <3ef4c1bb@opayq.com>
2019-07-18 03:13:57 -0500

    Refactor JDKDownloader and SystemUtils

--- src/main/java/com/github/coleb1911/ghost2/buildtools/JDKNotFoundException.java
+++ src/main/java/com/github/coleb1911/ghost2/buildtools/JDKNotFoundException.java
@@ -3,12 +3,12 @@ package com.github.coleb1911.ghost2.buildtools;
 /**
  * Thrown when {@link SystemUtils} can't find a valid JDK on the system
  */
-public final class JDKNotFoundException extends Exception {
-    private static final String DEFAULT_MESSAGE = "Could not find the JDK via JAVA_HOME or your PATH variable.\n" +
-            "Check to make sure one of these variables is set, contains a valid JDK location, and contains JDK binaries\n" +
+final class JDKNotFoundException extends Exception {
+    private static final String DEFAULT_MESSAGE = "Could not find the JDK via JAVA_HOME or your PATH variable." + System.lineSeparator() +
+            "Check to make sure one of these variables is set, contains a valid JDK location, and contains JDK binaries" + System.lineSeparator() +
             "for your platform that you have permission to execute.";
 
-    public JDKNotFoundException() {
+    JDKNotFoundException() {
         super(DEFAULT_MESSAGE);
     }
 }
---
+++ src/main/java/com/github/coleb1911/ghost2/shared/Constants.java
@@ -0,0 +1,5 @@
+package com.github.coleb1911.ghost2.shared;
+
+public class Constants {
+    public static final String VERSION_STRING = "0.1";
+}
