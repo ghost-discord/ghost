f641011157de575b7e6a957fd99d7b1b2248d5f9
CremBluRay <jhoggard2003@gmail.com>
2019-07-20 21:50:34 -0500

    add ban module

---
+++ src/main/java/com/github/coleb1911/ghost2/commands/modules/moderation/ModuleBan.java
@@ -0,0 +1,42 @@
+package com.github.coleb1911.ghost2.commands.modules.moderation;
+
+import com.github.coleb1911.ghost2.commands.meta.CommandContext;
+import com.github.coleb1911.ghost2.commands.meta.Module;
+import com.github.coleb1911.ghost2.commands.meta.ModuleInfo;
+import discord4j.core.object.util.Permission;
+import discord4j.core.object.util.PermissionSet;
+import reactor.core.publisher.Mono;
+
+public final class ModuleBan extends Module {
+    public ModuleBan() {
+        super(new ModuleInfo.Builder(ModuleBan.class)
+                .withName("ban")
+                .withDescription("Ban a desired user.")
+                .withBotPermissions(PermissionSet.of(Permission.BAN_MEMBERS)));
+    }
+
+    @Override
+    public void invoke(CommandContext ctx) {
+        //Check for args
+        try{
+            ctx.getArgs().get(0);
+        } catch(IndexOutOfBoundsException e) {
+            ctx.reply("Please specify a user.");
+            return;
+        }
+
+        String targetName = ctx.getArgs().get(0);
+        ctx.getGuild().getMembers()
+                .filter(member -> targetName.equals(member.getDisplayName()))
+                .map(member -> member.ban(spec -> {
+                    spec.setReason("Banned by " + ctx.getInvoker().getDisplayName());
+                    ctx.reply(member.getDisplayName() + " was banned.");
+                }).subscribe())
+                .hasElements()
+                .flatMap(aBoolean -> {
+                    if(!aBoolean) ctx.reply("User not found.");
+                    return Mono.just(aBoolean);
+                })
+                .subscribe();
+    }
+}
