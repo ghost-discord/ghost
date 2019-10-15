f90a47460599ed77de0495911022898e6681716e
Maxime Pardoen <Maxime.Pardoen@gmail.com>
2019-10-15 21:04:05 +0200

    Adds a textchannel option for the feature

--- src/main/java/com/github/coleb1911/ghost2/commands/modules/fun/ModuleRandomUser.java
+++ src/main/java/com/github/coleb1911/ghost2/commands/modules/fun/ModuleRandomUser.java
@@ -5,7 +5,11 @@ import com.github.coleb1911.ghost2.commands.meta.Module;
 import com.github.coleb1911.ghost2.commands.meta.ModuleInfo;
 import com.github.coleb1911.ghost2.commands.meta.ReflectiveAccess;
 import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.TextChannel;
 import discord4j.core.object.entity.User;
+import discord4j.core.object.presence.Status;
+import discord4j.core.object.util.Permission;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -25,28 +29,34 @@ public final class ModuleRandomUser extends Module {
 
     @Override
     public void invoke(@NotNull CommandContext ctx) {
-        VoiceState callerState = ctx.getInvoker().getVoiceState().block();
-        List<User> users = new ArrayList<>();
-        // If the invoker is currently in a voice channel
-        if(callerState !=null){
-            users = callerState
-                    .getChannel().block()
-                    .getVoiceStates().collectList().block()
-                    .stream().map(e -> e.getUser().block())
-                    .collect(Collectors.toList());
-        // If not, return all the users online in the current text channel
-        } else{
-            List<User> allUsers = ctx.getClient().getUsers().collectList().block();
-            for(User user : allUsers){
-                ctx.getInvoker().getBasePermissions().block().
-                user.
+        try{
+            VoiceState callerState = ctx.getInvoker().getVoiceState().block();
+            List<User> users = new ArrayList<>();
+            // If the invoker is currently in a voice channel
+            if(callerState !=null){
+                users = callerState
+                        .getChannel().block()
+                        .getVoiceStates().collectList().block()
+                        .stream().map(e -> e.getUser().block())
+                        .collect(Collectors.toList());
+                // If not, return a random user online in the current text channel
+            } else{
+                List<User> allUsers = ctx.getClient().getUsers().collectList().block();
+                for(User user : allUsers){
+                    Member member = user.asMember(ctx.getGuild().getId()).block();
+                    if(!user.isBot() && !member.getPresence().block().getStatus().equals(Status.OFFLINE) && ((TextChannel)ctx.getChannel()).getEffectivePermissions(user.getId()).block().asEnumSet().contains(Permission.VIEW_CHANNEL))
+                        users.add(user);
+                }
             }
+            if(!users.isEmpty()){
+                Random r = new Random();
+                User randomPick = users.get(r.nextInt(users.size()));
+                ctx.reply("The random user is "+randomPick.getUsername());
+            }else ctx.reply("There are no users to pick from");
+
+        } catch(NullPointerException e){
+            ctx.reply("Error when fetching information on the channel users");
         }
-        if(!users.isEmpty()){
-            Random r = new Random();
-            User randomPick = users.get(r.nextInt(users.size()));
-            ctx.reply("The random user is "+randomPick.getUsername());
-        }else ctx.reply("There are no users to pick from");
 
     }
 }
