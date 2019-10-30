a78f2bb5a39b676642784d96fd7aea108ff72353
Adam Ness <adam.ness@gmail.com>
2019-10-29 22:42:04 -0500

    feat: adding wikipedia command

---
+++ src/main/java/com/github/coleb1911/ghost2/commands/modules/fun/ModuleWikipedia.java
@@ -0,0 +1,178 @@
+package com.github.coleb1911.ghost2.commands.modules.fun;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.coleb1911.ghost2.commands.meta.CommandContext;
+import com.github.coleb1911.ghost2.commands.meta.Module;
+import com.github.coleb1911.ghost2.commands.meta.ModuleInfo;
+import discord4j.core.object.util.Permission;
+import discord4j.core.object.util.PermissionSet;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.jsoup.Jsoup;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.validation.constraints.NotNull;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.time.Instant;
+import java.util.List;
+
+/**
+ * Module for searching wikipedia and displaying the most recent result
+ */
+@Service
+public final class ModuleWikipedia extends Module {
+    private final URI baseURI;
+    private final RestTemplate restTemplate;
+
+    public ModuleWikipedia() {
+        this("https://en.wikipedia.org/", generateDefaultRestTemplate());
+    }
+
+    // https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Nelson%20Mandela&utf8=&format=json
+    private static RestTemplate generateDefaultRestTemplate() {
+        HttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build()).build();
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(client);
+        return new RestTemplate(clientHttpRequestFactory);
+    }
+
+    public ModuleWikipedia(String baseUrl, RestTemplate restTemplate) {
+        super(new ModuleInfo.Builder(ModuleWikipedia.class)
+            .withName("wikipedia")
+            .withAliases("wiki", "w")
+            .withDescription("Look up an article on Wikipedia")
+            .withBotPermissions(PermissionSet.of(Permission.EMBED_LINKS)));
+        this.baseURI = URI.create(baseUrl);
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public void invoke(@NotNull CommandContext ctx) {
+        String searchString = String.join(" ", ctx.getArgs());
+        String urlSearchString = URLEncoder.encode(searchString, Charset.defaultCharset());
+        String response;
+        URI resultQuery = baseURI.resolve("/w/api.php?action=query&list=search&utf8=&format=json&srsearch=" + urlSearchString);
+        try {
+            ResponseEntity<WikipediaSearchResults> results = restTemplate.getForEntity(resultQuery, WikipediaSearchResults.class);
+            SearchInfo searchInfo = results.getBody().getQueryResult().getSearchInfo();
+            List<SearchResult> searchResults = results.getBody().getQueryResult().getSearchResults();
+            if( !searchResults.isEmpty() ) {
+                SearchResult topResult = searchResults.get(0);
+                ctx.getChannel().createMessage(message -> message.setEmbed(embedCreateSpec -> {
+                    embedCreateSpec.setTitle(topResult.title);
+                    embedCreateSpec.setImage("https://en.wikipedia.org/static/images/project-logos/enwiki-2x.png");
+                    embedCreateSpec.setDescription(Jsoup.parse(topResult.snippet).text());
+                })).block();
+            } else {
+                if( searchInfo.hasSuggestions() ) {
+                    ctx.reply("Did you mean \"" + searchInfo.getSuggestion() + "\"?");
+                } else {
+                    ctx.reply("Couldn't find anything about \"" + searchString + "\" on wikipedia.");
+                }
+            }
+        } catch (RestClientException ex ) {
+            response = "Error: " + ex.toString();
+            ctx.reply(response);
+        }
+    }
+
+    private static class SearchInfo {
+        @JsonProperty(value="totalHits")
+        private int totalHits;
+
+        @JsonProperty(value = "suggestion")
+        private String suggestion;
+
+        @JsonProperty(value="suggestionsnippet")
+        private String suggestionSnippet;
+
+        public String getSuggestion() {
+            return suggestion;
+        }
+
+        public boolean hasSuggestions() {
+            return suggestion != null && !suggestion.isBlank();
+        }
+
+        public boolean hasResults() {
+            return totalHits >= 1;
+        }
+    }
+
+    private static class SearchResult {
+        @JsonProperty(value="ns")
+        private int ns;
+
+        @JsonProperty(value="title")
+        private String title;
+
+        @JsonProperty(value="pageid")
+        private int pageId;
+
+        @JsonProperty(value="size")
+        private int size;
+
+        @JsonProperty(value="wordcount")
+        private int wordCount;
+
+        @JsonProperty(value="snippet")
+        private String snippet;
+
+        @JsonProperty(value="timestamp")
+        private Instant timestamp;
+
+        public String getSnippet() {
+            return snippet;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class QueryResult {
+        @JsonProperty(value="searchinfo")
+        private SearchInfo searchInfo;
+
+        @JsonProperty(value="search")
+        private List<SearchResult> searchResults;
+
+        public List<SearchResult> getSearchResults() {
+            return searchResults;
+        }
+
+        public SearchInfo getSearchInfo() {
+            return searchInfo;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class ContinueObject {
+        @JsonProperty(value="sroffset")
+        private int srOffset;
+
+        @JsonProperty(value="continue")
+        private String continueString;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class WikipediaSearchResults {
+        @JsonProperty(value="batchcomplete")
+        private String batchComplete;
+
+        @JsonProperty(value="continue")
+        private ContinueObject continueObject;
+
+        @JsonProperty(value="query")
+        private QueryResult queryResult;
+
+        public QueryResult getQueryResult() {
+            return queryResult;
+        }
+    }
+}
---
+++ src/test/java/com/github/coleb1911/ghost2/commands/modules/fun/ModuleWikipediaTest.java
@@ -0,0 +1,82 @@
+package com.github.coleb1911.ghost2.commands.modules.fun;
+
+import com.github.coleb1911.ghost2.commands.meta.CommandContext;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ModuleWikipediaTest {
+    WireMockServer wireMockServer;
+
+    @BeforeEach
+    public void setup() {
+        wireMockServer = new WireMockServer(8090);
+        wireMockServer.start();
+        setupStub();
+    }
+
+    private void setupStub() {
+        wireMockServer.stubFor(get(urlPathEqualTo("/w/api.php"))
+            .withQueryParam("srsearch", equalTo("Azmec"))
+            .withQueryParam( "action", equalTo("query"))
+            .willReturn(aResponse().withHeader("Content-Type", "application/json")
+            .withStatus(200)
+            .withBodyFile("json/azmec.json")));
+        wireMockServer.stubFor(get(urlPathEqualTo("/w/api.php"))
+            .withQueryParam("srsearch", equalTo("Nelson Mandela"))
+            .withQueryParam( "action", equalTo("query"))
+            .willReturn(aResponse().withHeader("Content-Type", "application/json")
+            .withStatus(200)
+            .withBodyFile("json/nelson-mandela.json")));
+
+    }
+
+    @AfterEach
+    public void teardown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    public void testWikiNeslonMandela() {
+        final RestTemplate restTemplate = new RestTemplate();
+        ModuleWikipedia wikiModule = new ModuleWikipedia(wireMockServer.baseUrl(), restTemplate );
+        CommandContext mockContext = mock(CommandContext.class, RETURNS_DEEP_STUBS);
+        when(mockContext.getArgs()).thenReturn(List.of("Nelson Mandela"));
+        ArgumentCaptor<Consumer<? super MessageCreateSpec>> messageCreatorCaptor = ArgumentCaptor.forClass(Consumer.class);
+        wikiModule.invoke(mockContext);
+        verify(mockContext.getChannel()).createMessage(messageCreatorCaptor.capture());
+
+        MessageCreateSpec mockCreateSpec = mock(MessageCreateSpec.class);
+        ArgumentCaptor<Consumer<? super EmbedCreateSpec>> embedCreatorCaptor = ArgumentCaptor.forClass(Consumer.class);
+        messageCreatorCaptor.getValue().accept(mockCreateSpec);
+        verify(mockCreateSpec).setEmbed(embedCreatorCaptor.capture());
+    }
+
+    @Test
+    public void testSuggestion() {
+        final RestTemplate template = new RestTemplate();
+        ModuleWikipedia wikiModule = new ModuleWikipedia(wireMockServer.baseUrl(), template);
+        CommandContext mockContext = mock(CommandContext.class, RETURNS_DEEP_STUBS);
+        when(mockContext.getArgs()).thenReturn(List.of("Azmec"));
+        wikiModule.invoke(mockContext);
+
+        ArgumentCaptor<String> reply = ArgumentCaptor.forClass(String.class);
+        verify(mockContext).reply(reply.capture());
+        assertEquals("Did you mean \"aztecs\"?", reply.getValue());
+
+
+    }
+}
