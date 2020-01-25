d25fc11809bbcf76d2e887c21682a72b30ffc1ea
cbryant02 <3ef4c1bb@opayq.com>
2020-01-24 23:16:01 -0600

    Resolve warnings in GuildMetaRepository

--- src/main/java/com/github/coleb1911/ghost2/database/repos/GuildMetaRepository.java
+++ src/main/java/com/github/coleb1911/ghost2/database/repos/GuildMetaRepository.java
@@ -2,10 +2,8 @@ package com.github.coleb1911.ghost2.database.repos;
 
 import com.github.coleb1911.ghost2.commands.meta.ReflectiveAccess;
 import com.github.coleb1911.ghost2.database.entities.GuildMeta;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -16,23 +14,23 @@ import java.util.Optional;
 @Component
 public interface GuildMetaRepository extends CrudRepository<GuildMeta, Long> {
     @Override
-    @CachePut("guilds")
-    <S extends GuildMeta> S save(S entity);
+    @NonNull
+    <S extends GuildMeta> S save(@NonNull S entity);
 
     @Override
-    @Cacheable(value = "guilds", key = "#p0")
-    Optional<GuildMeta> findById(Long id);
+    @NonNull
+    Optional<GuildMeta> findById(@NonNull Long id);
 
     @Override
+    @NonNull
     Iterable<GuildMeta> findAll();
 
     @Override
     long count();
 
     @Override
-    @CacheEvict(value = "guilds", key = "#p0.id")
-    void delete(GuildMeta guild);
+    void delete(@NonNull GuildMeta guild);
 
     @Override
-    boolean existsById(Long id);
+    boolean existsById(@NonNull Long id);
 }
